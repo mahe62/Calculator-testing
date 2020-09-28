8e9d9e1756ffba8b139ef231c266416b1d7de20c
Rizqi Mahendra <mahee6232@gmail.com>
2020-09-28 12:29:33 +0700

    parseInt fix

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -4,9 +4,15 @@
 
 const functions = require('../helpers');
 
-test('1 tambah "1" adalah 2', ()=> {
+test('1 tambah 1 adalah 2', ()=> {
     expect(functions.incrementQty(1)).toBe(2);
 });
-test('4 kurang "1" adalah 3', ()=> {
+test('4 kurang 1 adalah 3', ()=> {
     expect(functions.decrementQty(4)).toBe(3);
+});
+test('"1" tambah 1 adalah 2', ()=> {
+    expect(functions.incrementQty("1")).toBe(2);
+});
+test('"4" kurang 1 adalah 3', ()=> {
+    expect(functions.decrementQty("4")).toBe(3);
 });
