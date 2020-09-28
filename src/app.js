59e9e77a27bec864d3b5115baab6215de8fa176d
Rizqi Mahendra <mahee6232@gmail.com>
2020-09-28 16:27:49 +0700

    bayar

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import {
   decrementQty,
   total,
   kode,
+  pay,
+  pd
 } from './helpers.js';
 
 const incrButton = document.querySelector('#incr');
@@ -11,15 +13,27 @@ const qtyInput = document.querySelector('#qty');
 const price = document.querySelector('#price');
 const subtotal = document.querySelector('#subtotal');
 const code = document.querySelector('#code');
+const bayar = document.querySelector('#bayar');
+const sisa = document.querySelector('#sisa');
 
 incrButton.addEventListener('click', () => {
   qtyInput.value = incrementQty(qtyInput.value);
   subtotal.textContent = `Rp. ${total(price.value, qtyInput.value)}`;
+  btnBayar.addEventListener('click',()=>{
+    sisa.textContent = `Rp. ${pay(bayar.value, price.value, qtyInput.value)}`;
+  });
 });
 
 decrButton.addEventListener('click', () => {
-  qtyInput.value = decrementQty(qtyInput.value);
+  if (qtyInput.value<2) {
+    qtyInput.value = 1;
+  }else{
+    qtyInput.value = decrementQty(qtyInput.value);
+  }
   subtotal.textContent = `Rp. ${total(price.value, qtyInput.value)}`;
+  btnBayar.addEventListener('click',()=>{
+    sisa.textContent = `Rp. ${pay(bayar.value, price.value, qtyInput.value)}`;
+  });
 
 });
 
@@ -27,12 +41,21 @@ confirmButton.addEventListener('click',()=>{
 if (code.value.toUpperCase() == "PPL50") {
   var dis = 50;
       subtotal.textContent = `Rp. ${kode(price.value, qtyInput.value,dis)}`
+      btnBayar.addEventListener('click',()=>{
+        sisa.textContent = `Rp. ${pd(bayar.value, price.value, qtyInput.value,dis)}`;
+      });
 }else if (code.value.toUpperCase()== "PPL20") {
   var dis = 20;
   subtotal.textContent = `Rp. ${kode(price.value, qtyInput.value,dis)}`;
+  btnBayar.addEventListener('click',()=>{
+    sisa.textContent = `Rp. ${pd(bayar.value, price.value, qtyInput.value,dis)}`;
+  });
 }else{
   subtotal.textContent = `Rp. ${total(price.value, qtyInput.value)}`;
-
+  btnBayar.addEventListener('click',()=>{
+    sisa.textContent = `Rp. ${pay(bayar.value, price.value, qtyInput.value)}`;
+  });
 }
 // subtotal.textContent = `Rp. ${kode(price.value, qtyInput.value, code.value)}`;
 });
+
--- src/helpers.js
+++ src/helpers.js
@@ -2,10 +2,14 @@ const incrementQty = qty => parseInt(qty) + 1;
 const decrementQty = qty => parseInt(qty) - 1;
 const total = (price, qty) => qty*price;
 const kode = (price, qty, dis) => qty*price - dis/100*(price*qty);
+const pay = (bayar,price, qty)=> bayar-qty*price;
+const pd = (price, qty, dis) => bayar -(qty*price - dis/100*(price*qty));
 
 module.exports = { 
     incrementQty, 
     decrementQty,
     total,
-    kode
+    kode,
+    pay,
+    pd
 };
