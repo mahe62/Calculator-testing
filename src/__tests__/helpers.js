845564c84eceb27b1c65d5302361d01ac204a55d
Rizqi Mahendra <mahee6232@gmail.com>
2020-09-28 12:25:55 +0700

    parse int

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -2,3 +2,11 @@
     Gunakan file ini untuk membuat test case dengan Jest
 */
 
+const functions = require('../helpers');
+
+test('1 tambah "1" adalah 2', ()=> {
+    expect(functions.incrementQty(1)).toBe(2);
+});
+test('4 kurang "1" adalah 3', ()=> {
+    expect(functions.decrementQty(4)).toBe(3);
+});
